7b8a17cfab792f661b90f5b4e83aa1b19c7eb9aa
Thiago Marques <thiagomarques408@gmail.com>
2023-05-21 18:21:40 -0300

    feat: exibe lista de compromissos

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private CompromissoDB mCompromissoDB;
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         this.horaSelecionada = horaSelecionada;
     }
 
+    private TextView mCompromissosTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String descricao = mDescricao.getText().toString();
                 criaCompromisso(dataSelecionada, horaSelecionada, descricao);
+            }
+        });
+
+        Button botaoHoje = findViewById(R.id.btn_hoje);
+        mCompromissosTextView = findViewById(R.id.textView_scroll);
+
+        botaoHoje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostraCompromissos();
             }
         });
     }
@@ -56,6 +70,29 @@ public class MainActivity extends AppCompatActivity {
 
         Compromisso compromisso = new Compromisso(data, hora, descricao);
         mCompromissoDB.addCompromisso(compromisso);
+    }
+
+    private void mostraCompromissos() {
+        if (mCompromissoDB == null) return;
+
+        Cursor cursor = mCompromissoDB.queryCompromissos(null, null);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                String data = cursor.getString(cursor.getColumnIndexOrThrow(CompromissosDBSchema.CompromissosTbl.Cols.DATA));
+                String hora = cursor.getString(cursor.getColumnIndexOrThrow(CompromissosDBSchema.CompromissosTbl.Cols.HORA));
+                String descricao = cursor.getString(cursor.getColumnIndexOrThrow(CompromissosDBSchema.CompromissosTbl.Cols.DESCRICAO));
+
+                if (hora != null && !descricao.isEmpty()) {
+                    stringBuilder.append(data).append(" - ").append(hora).append(" - ").append(descricao).append("\n");
+                }
+            } while (cursor.moveToNext());
+
+            cursor.close();
+        }
+
+        mCompromissosTextView.setText(stringBuilder.toString());
     }
 
 }
